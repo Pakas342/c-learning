7815c069b18fe6e8626ed194630cd7a86acbd810
Pakas342 <jcbp1999@gmail.com>
2026-03-13 23:55:12 +1100

    exercises

--- ed-lesson-3/random_password.c
+++ ed-lesson-3/random_password.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define min_ascii '!'
+#define max_ascii '~'
+
+int main(int argc, char **argv) {
+    int lenght = atoi(argv[1]);
+    int valid_interval = max_ascii - min_ascii;
+    srand(time(NULL));
+
+    for (int i=0; i<lenght; i++){
+         char random_char  = rand() % valid_interval + 1 + min_ascii;
+        printf("%c", random_char);
+    }
+    printf("\n");
+}
